32e083cbd52ed4bccd5e7e07c6e3df1884f831f8
Werner Strydom <werners@bloudraak.com>
2018-03-11 16:15:32 -0700

    Build

--- build.cmd
+++ build.cmd
@@ -1,11 +1,12 @@
 @echo off
+
 mkdir build
 pushd build
 mkdir win64
 pushd win64 
-pushd "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\VC\Auxiliary\Build"
-call vcvarsall /clean_env
-call vcvarsall.bat amd64
+pushd %VS140COMNTOOLS%
+call vsdevcmd.bat /clean_env 
+call vsdevcmd.bat amd64
 popd
 cmake -G "NMake Makefiles" ../.. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../../dist/win64/ -DCPACK_GENERATOR=WIX;ZIP;TGZ
 nmake install
@@ -14,14 +15,17 @@ popd
 
 mkdir win32
 pushd win32
-pushd "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\VC\Auxiliary\Build"
-call vcvarsall /clean_env
-call vcvarsall.bat amd64_x86
+pushd %VS140COMNTOOLS%
+call vsdevcmd.bat /clean_env 
+call vsdevcmd.bat amd64_x86
 popd
 cmake -G "NMake Makefiles" ../.. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../../dist/win32/ -DCPACK_GENERATOR=WIX;ZIP;TGZ
 nmake install
 nmake package
 popd
+popd
 
+pushd %VS140COMNTOOLS%
+call vsdevcmd.bat /clean_env 
 popd
 
