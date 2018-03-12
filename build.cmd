8e017aa26dbdc4968413107468ad98592b8d10ed
Werner Strydom <werners@bloudraak.com>
2018-03-11 18:28:53 -0700

    Build

--- build.cmd
+++ build.cmd
@@ -6,9 +6,9 @@ mkdir build
 pushd build
 mkdir win64
 pushd win64 
-pushd %VS150COMNTOOLS%
-call vsdevcmd.bat /clean_env 
-call vsdevcmd.bat amd64
+pushd "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build"
+call vcvarsall.bat /clean_env
+call vcvarsall.bat amd64 
 popd
 cmake -G "NMake Makefiles" ../.. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../../dist/win64/ -DCPACK_GENERATOR=WIX;ZIP;TGZ
 nmake install
@@ -17,9 +17,9 @@ popd
 
 mkdir win32
 pushd win32
-pushd %VS150COMNTOOLS%
-call vsdevcmd.bat /clean_env 
-call vsdevcmd.bat amd64_x86
+pushd "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build"
+call vcvarsall.bat /clean_env
+call vcvarsall.bat amd64_x86
 popd
 cmake -G "NMake Makefiles" ../.. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../../dist/win32/ -DCPACK_GENERATOR=WIX;ZIP;TGZ
 nmake install
@@ -27,7 +27,6 @@ nmake package
 popd
 popd
 
-pushd %VS150COMNTOOLS%
-call vsdevcmd.bat /clean_env 
+pushd "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build"
+call vcvarsall.bat /clean_env
 popd
-
