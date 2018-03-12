0db5c6deb63b5079c6eaf426c698e7bdc42bcb70
Werner Strydom <werners@bloudraak.com>
2018-03-11 18:46:16 -0700

    Build

--- build.cmd
+++ build.cmd
@@ -1,13 +1,10 @@
 @echo off
 
-set 
-
 mkdir build
 pushd build
 mkdir win64
 pushd win64 
-pushd "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build"
-call vcvarsall.bat /clean_env
+pushd "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\"
 call vcvarsall.bat amd64 
 popd
 cmake -G "NMake Makefiles" ../.. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../../dist/win64/ -DCPACK_GENERATOR=WIX;ZIP;TGZ
@@ -18,15 +15,11 @@ popd
 mkdir win32
 pushd win32
 pushd "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build"
-call vcvarsall.bat /clean_env
-call vcvarsall.bat amd64_x86
+call vcvarsall.bat amd64_x86 
 popd
 cmake -G "NMake Makefiles" ../.. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../../dist/win32/ -DCPACK_GENERATOR=WIX;ZIP;TGZ
 nmake install
 nmake package
 popd
-popd
 
-pushd "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build"
-call vcvarsall.bat /clean_env
 popd
